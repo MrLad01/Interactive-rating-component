804bd4a2464e1fe573099141a9de342d515caa7f
MrLad01 <ladayodeji@gmail.com>
2023-10-09 21:33:59 +0100

    update

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -2,7 +2,7 @@
 function App() {
   return (
     <main className="w-scren h-screen flex justify-center items-center">
-      <div className="w-1/3 h-1/2 bg-black"></div>
+      <div className="w-1/3 h-1/2 bg-black rounded-xl"></div>
     </main>
   )
 }
